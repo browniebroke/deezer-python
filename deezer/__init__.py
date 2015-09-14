8b9c1eaaee7f491df1d0094f3a1437a7f7ec6cbe
Bruno Alla <bruno.alla@founders4schools.org.uk>
2015-09-14 20:25:26 +0100

    Bump version

--- deezer/__init__.py
+++ deezer/__init__.py
@@ -7,7 +7,7 @@ do some searches, and build application written in Python on top of it
 
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.2'
 __all__ = ['AsyncClient', 'Client', 'Resource', 'Album',
            'Artist', 'Genre', 'Playlist', 'Track', 'User',
            'Comment', 'Radio']
