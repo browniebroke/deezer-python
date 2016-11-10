04cd17bb03f2b15cf37313cb3261dd37902d82b0
Bruno Alla <bruno.alla@founders4schools.org.uk>
2016-11-10 10:30:24 +0000

    Add a check that coveralls is actually called

--- run_coveralls.py
+++ run_coveralls.py
@@ -7,5 +7,6 @@ from subprocess import call
 
 if __name__ == '__main__':
     if 'TRAVIS' in os.environ:
+        print("Calling coveralls")
         rc = call('coveralls')
         raise SystemExit(rc)
