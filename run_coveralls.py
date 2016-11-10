f62ed7eabe871451c88058edfc570e6f1dc1c355
Bruno Alla <bruno.alla@founders4schools.org.uk>
2016-11-10 10:33:16 +0000

    Remove debug statement

--- run_coveralls.py
+++ run_coveralls.py
@@ -7,6 +7,5 @@ from subprocess import call
 
 if __name__ == '__main__':
     if 'TRAVIS' in os.environ:
-        print("Calling coveralls")
         rc = call('coveralls')
         raise SystemExit(rc)
