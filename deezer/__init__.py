534ed7fcdafa6aeda675c9d12426ddce4645fa31
Bruno Alla <bruno.alla@founders4schools.org.uk>
2016-12-08 11:44:52 +0000

    Bump version: 0.3.0 → 0.4.0

--- deezer/__init__.py
+++ deezer/__init__.py
@@ -12,7 +12,7 @@ from deezer.resources import Album, Resource, Artist, Playlist
 from deezer.resources import Genre, Track, User, Comment, Radio
 
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __all__ = ['AsyncClient', 'Client', 'Resource', 'Album',
            'Artist', 'Genre', 'Playlist', 'Track', 'User',
            'Comment', 'Radio']
