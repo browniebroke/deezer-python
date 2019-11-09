71ec93b216a308616e46c4f9396827e4b4a63798
Bruno Alla <alla.brunoo@gmail.com>
2019-11-09 15:35:29 +0000

    Bump version: 1.2.0 → 1.3.0

--- deezer/__init__.py
+++ deezer/__init__.py
@@ -17,7 +17,7 @@ from deezer.resources import (
     User,
 )
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __all__ = [
     "Client",
     "Resource",
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*paths):
         return f.read()
 
 
-version = "1.2.0"
+version = "1.3.0"
 
 setup(
     name="deezer-python",
