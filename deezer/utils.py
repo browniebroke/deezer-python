765b898a990c8cf6c2490aa2b58afd1046633bdf
Bruno Alla <alla.brunoo@gmail.com>
2020-05-02 17:36:49 +0100

    Typo

--- deezer/utils.py
+++ deezer/utils.py
@@ -7,7 +7,7 @@ class SortedDict(OrderedDict):
     """Sorted ``dict``.
 
     This class is present to order query string and keep the request call always the
-    same way. This is necessary to have reproductible tests with ``vcr.py``.
+    same way. This is necessary to have reproducible tests with ``vcr.py``.
 
     ``dict`` ordering is deterministic before 3.6 and is not guaranteed in Python < 3.7
     """
