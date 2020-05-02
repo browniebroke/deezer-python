9433b40d7713588b3852850a3ca532acb8dbcf9f
Bruno Alla <alla.brunoo@gmail.com>
2020-05-02 16:24:32 +0100

    Mark a few arguments of the Client class as deprecated
    These don't seem too useful, so deprecating them
    
    - host: quite unlikely to change
    - use_ssl: we should default to True
    - do_not_compress_reponse: limited use in tests

--- tests/base.py
+++ tests/base.py
@@ -6,9 +6,7 @@ import deezer
 class BaseTestCaseWithVcr(vcr_unittest.VCRTestCase):
     def setUp(self):
         super().setUp()
-        self.client = deezer.Client(  # nosec
-            app_id="foo", app_secret="bar", do_not_compress_reponse=True
-        )
-        self.unsec_client = deezer.Client(use_ssl=False, do_not_compress_reponse=True)
+        self.client = deezer.Client(app_id="foo", app_secret="bar")  # nosec
+        self.unsec_client = deezer.Client(use_ssl=False)
         self.client_fr = deezer.Client(headers={"Accept-Language": "fr"})  # French
         self.client_ja = deezer.Client(headers={"Accept-Language": "ja"})  # Japanese
