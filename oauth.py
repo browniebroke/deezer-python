0f043a9facbe8dad86ab763e0652a727bf4dfefc
Carababusha <carababusha@gmail.com>
2021-10-11 21:43:33 +0100

    initial test of env file token

--- oauth.py
+++ oauth.py
@@ -88,6 +88,14 @@ class MyServer(BaseHTTPRequestHandler):
             f"<br>"
             f"Expires = {token_data['expires']}"
         )
+
+        """Write token to .env file"""
+        with open(".env", "w+") as file:
+            file.writelines(["API_TOKEN="+token_data['access_token']+'\n'])
+            print ("Token written to .env file") 
+
+        file.close()
+
         raise SystemExit("All Done")
 
     def _render_content(self, content: str):
