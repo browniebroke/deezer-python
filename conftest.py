57ec23baffa3a257d717b5f69fa1b2bc702f0969
Bruno Alla <alla.brunoo@gmail.com>
2021-10-16 15:51:27 +0100

    test: clean response before saving cassettes

--- conftest.py
+++ conftest.py
@@ -23,8 +23,19 @@ def client_token(client):
     return client
 
 
+def clean_response(response):
+    """Remove a few info from the response before writing cassettes."""
+    if not isinstance(response, dict):
+        return response
+    response["headers"].pop("Set-Cookie", None)
+    response["headers"].pop("Date", None)
+    response["headers"].pop("P3P", None)
+    return response
+
+
 @pytest.fixture(scope="module", autouse=True)
 def vcr_config():
     return {
         "filter_query_parameters": [("access_token", "dummy")],
+        "before_record_response": clean_response,
     }
