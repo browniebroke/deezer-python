d69eef08ff0e661a81814742e0b94d91acfd32db
Bruno Alla <alla.brunoo@gmail.com>
2021-10-17 11:51:31 +0100

    test: write more tests for the AsyncClient

--- tests/test_tornado_client.py
+++ tests/test_tornado_client.py
@@ -1,6 +1,5 @@
 import pytest
-import tornado.gen
-import tornado.ioloop
+from tornado.httpclient import AsyncHTTPClient
 
 from deezer import Album
 from deezer.contrib.tornado import AsyncClient
@@ -9,14 +8,28 @@ pytestmark = pytest.mark.vcr
 
 
 class TestAsyncClient:
-    def test_get_object(self):
-        client = AsyncClient()
+    @pytest.fixture()
+    def async_client(self):
+        return AsyncClient()
 
-        @tornado.gen.coroutine
-        def callback():
-            album = yield client.get_album(302127)
-            assert isinstance(album, Album)
-            tracks = yield album.get_tracks()
-            assert tracks[0].album is album
+    @pytest.mark.gen_test
+    def test_access_token_set(self, async_client, mocker):
+        client_fetch = mocker.spy(AsyncHTTPClient, "fetch")
+        async_client.access_token = "dummy"
+        yield async_client.request("GET", "user/me")
+        client_fetch.assert_called_with(
+            mocker.ANY,
+            "https://api.deezer.com/user/me?access_token=dummy",
+            method="GET",
+        )
 
-        tornado.ioloop.IOLoop.instance().run_sync(callback)
+    @pytest.mark.gen_test
+    def test_get_object(self, async_client):
+        album = yield async_client.get_album(302127)
+        assert isinstance(album, Album)
+
+    @pytest.mark.gen_test
+    def test_get_relation(self, async_client):
+        album = Album(async_client, {"id": 302127, "type": "album"})
+        tracks = yield album.get_tracks()
+        assert tracks[0].album is album
