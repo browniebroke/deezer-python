f38a03812c73f74eedf8077ffd29a1b2b7a67188
Bruno Alla <browniebroke@users.noreply.github.com>
2022-07-26 21:10:21 +0100

    feat: infer episode link & share from ID when missing (#544)

--- tests/resources/test_episode.py
+++ tests/resources/test_episode.py
@@ -19,3 +19,28 @@ class TestEpisode:
         episode_dict = episode.as_dict()
         assert episode_dict["id"] == 343457312
         assert episode_dict["release_date"] == "2021-11-22 23:42:00"
+
+    def test_access_inferable_fields(self, client):
+        """Accessing a missing inferable field doesn't do any API calls."""
+        episode = deezer.Episode(
+            client,
+            json={
+                "id": 343457312,
+                "type": "episode",
+            },
+        )
+        assert episode.link == "https://www.deezer.com/episode/343457312"
+        assert episode.share == (
+            "https://www.deezer.com/episode/343457312?utm_source=deezer"
+            "&utm_content=episode-343457312&utm_medium=web"
+        )
+
+    def test_access_non_inferable_field(self, client):
+        episode = deezer.Episode(
+            client,
+            json={
+                "id": 343457312,
+                "type": "episode",
+            },
+        )
+        assert episode.duration == 3254
--- tests/resources/test_resource.py
+++ tests/resources/test_resource.py
@@ -14,16 +14,32 @@ class TestResource:
         tracks = album.get_tracks()
         assert tracks[0].album is album
 
-    def test_access_field_shallow_object(self, client):
-        """Accessing a field of shallow object fetches the full object."""
-        episode = deezer.Episode(
+    def test_access_non_inferable_field_simplified_objet(self, client):
+        """Fetch the full object when the missing field is not inferable."""
+        track = deezer.Track(
             client,
             json={
-                "id": 343457312,
-                "type": "episode",
+                "id": 3135556,
+                "type": "track",
             },
         )
-        assert episode.link == "https://www.deezer.com/episode/343457312"
+        assert track.bpm == 123.4
+
+    def test_access_no_infinite_fetch(self, client):
+        track = deezer.Track(
+            client,
+            json={
+                "id": 3135556,
+                "type": "track",
+            },
+        )
+        # Does and API call
+        assert track.title == "Harder, Better, Faster, Stronger"
+
+        # Response cassette has been modified to simulate missing 'bpm' field
+        with pytest.raises(AttributeError) as exc_info:
+            track.bpm
+        assert str(exc_info.value) == "'Track' object has no attribute 'bpm'"
 
     def test_field_not_found(self, client):
         """When field is missing an attribute error is raised without API calls."""
